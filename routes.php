6d74748771aad35c8272b106436c3cef25d274b8
Miguel Sánchez <aleminapavon30@gmail.com>
2025-03-12 14:39:04 -0600

    Defined host

--- routes.php
+++ routes.php
@@ -3,18 +3,18 @@ require_once __DIR__ . "/app/controllers/UsuarioController.php";
 require_once __DIR__ . "/app/core/Router.php";
 require_once __DIR__ . "/app/controllers/AuthController.php";
 
-$host = "";
+$host = "https://api-registro.azurewebsites.net/";
 
 
 $router = new Router;
 
-$router->addRoute("GET", "/users", "UsuarioController", "getAllUsers");
-$router->addRoute("GET", "/users/{id}", "UsuarioController", "getOneUser");
-$router->addRoute("POST", "/users", "UsuarioController", "createUser");
+$router->addRoute("GET", `{$host}/users`, "UsuarioController", "getAllUsers");
+$router->addRoute("GET", `{$host}/users/{id}`, "UsuarioController", "getOneUser");
+$router->addRoute("POST", `{$host}/users`, "UsuarioController", "createUser");
 
-$router->addRoute("POST", "/login", "AuthController", "login");
+$router->addRoute("POST", `{$host}/login`, "AuthController", "login");
 
-$router->addRoute("GET", "/profile", "UsuarioController", "getProfile");
+$router->addRoute("GET", `{$host}/profile`, "UsuarioController", "getProfile");
 
 $router->dispatch($_SERVER["REQUEST_METHOD"], explode("?", $_SERVER["REQUEST_URI"])[0]);
 ?>
