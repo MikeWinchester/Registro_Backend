754dbd9f68bb1ecd8ad780363fa672c6d879edad
Miguel Sánchez <aleminapavon30@gmail.com>
2025-03-12 21:47:32 -0600

    test 2

--- routes.php
+++ routes.php
@@ -3,7 +3,7 @@ require_once __DIR__ . "/app/controllers/UsuarioController.php";
 require_once __DIR__ . "/app/core/Router.php";
 require_once __DIR__ . "/app/controllers/AuthController.php";
 
-$host = "https://api-registro-faeaeyafg8fdfwdj.centralus-01.azurewebsites.net/";
+$host = "https://api-registro-faeaeyafg8fdfwdj.centralus-01.azurewebsites.net";
 
 
 $router = new Router;
