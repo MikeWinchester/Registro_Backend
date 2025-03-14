325889169589c2579a29a0fff93e72148b5af038
Miguel Sánchez <aleminapavon30@gmail.com>
2025-03-13 18:57:49 -0600

    testing 32

--- startup.sh
+++ startup.sh
@@ -2,6 +2,3 @@
 
 # Ejecutar el script para configurar NGINX y reiniciar el servicio
 /home/site/wwwroot/start_nginx.sh
-
-# Mantener el contenedor en ejecución (esto es importante en Azure App Service)
-tail -f /dev/null
