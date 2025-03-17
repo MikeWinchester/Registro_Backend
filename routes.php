883bd9600618576681c8e8f80fdbe5849b5ec904
Roberto Matamoros <65750735+Hunter774@users.noreply.github.com>
2025-03-16 22:49:50 -0600

    Add files via upload

--- routes.php
+++ routes.php
@@ -3,6 +3,7 @@ require_once __DIR__ . "/app/controllers/UsuarioController.php";
 require_once __DIR__ . "/app/core/Router.php";
 require_once __DIR__ . "/app/controllers/AuthController.php";
 require_once __DIR__ . "/app/controllers/DocenteController.php";
+require_once __DIR__ . "/app/controllers/AdmisionesController.php";
 require_once __DIR__ . "/app/controllers/SeccionesController.php";
 require_once __DIR__ . "/app/controllers/MatriculaController.php";
 
@@ -26,6 +27,8 @@ $router->addRoute("GET", "/secciones/{id}", "SeccionesController", "getSeccion")
 $router->addRoute("GET", "/secciones/matricula/", "SeccionesController", "getSeccionAsig");
 
 $router->addRoute("GET", "/matricula/estudiantes/{id}", "MatriculaController", "getEstudiantes");
+
+$router->addRoute("POST", "/admisiones", "AdmisionesController", "createAdmision");
 
 $router->dispatch($_SERVER["REQUEST_METHOD"], $_SERVER["REQUEST_URI"]);
 ?>
