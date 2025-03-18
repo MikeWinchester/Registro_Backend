aed779d9151938f129a8bd78ffa77d71d487137d
Roberto Matamoros <65750735+Hunter774@users.noreply.github.com>
2025-03-18 01:23:40 -0600

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . "/app/controllers/UsuarioController.php";
 require_once __DIR__ . "/app/core/Router.php";
+require_once __DIR__ . "/app/controllers/AuthController.php";
 require_once __DIR__ . "/app/controllers/DocenteController.php";
 require_once __DIR__ . "/app/controllers/AdmisionesController.php";
 require_once __DIR__ . "/app/controllers/SeccionesController.php";
