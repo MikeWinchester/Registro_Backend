7d833e977e453c6b06b6eece3b6d1dc8d22fc930
Roberto Matamoros <65750735+Hunter774@users.noreply.github.com>
2025-03-17 22:27:47 -0600

    Add files via upload

--- app/models/Admisiones.php
+++ app/models/Admisiones.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/../core/Model.php";
 class Admisiones extends Model {
 
     public function __construct() {
-        parent::__construct("usuario"); // La tabla que estamos usando
+        parent::__construct("admision");
     }
 
 }
