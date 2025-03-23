295c99a7e177d093ed00e2b8fbf6decd943b1180
Gerardo Rodriguez <55673748+g3rrarrd@users.noreply.github.com>
2025-03-22 21:34:03 -0600

    Add files via upload
    modified:   routes.php

--- routes.php
+++ routes.php
@@ -9,6 +9,7 @@ require_once __DIR__ . "/app/controllers/MatriculaController.php";
 require_once __DIR__ . "/app/controllers/CarreraController.php";
 require_once __DIR__ . "/app/controllers/CentroController.php";
 require_once __DIR__ . "/app/controllers/NotasController.php";
+require_once __DIR__ . "/app/controllers/ClaseController.php";
 
 $router = new Router;
 
@@ -28,10 +29,12 @@ $router->addRoute("GET", "/docentes", "DocenteController", "getAllDocentes");
 $router->addRoute("POST", "/docentes/seccion", "DocenteController", "uploadVideo");
 
 //Routes for Secciones
-$router->addRoute("GET", "/secciones/docente/{idDocente}", "SeccionesController", "getSecciones");
+$router->addRoute("GET", "/secciones/docente/all", "SeccionesController", "getSecciones");
+$router->addRoute("GET", "/secciones/docente", "SeccionesController", "getSeccionesActuales");
 $router->addRoute("GET", "/secciones/{idSeccion}", "SeccionesController", "getSeccion");
-$router->addRoute("GET", "/secciones/matricula/", "SeccionesController", "getSeccionAsig");
-$router->addRoute("GET", "/secciones/count/{idDocente}", "SeccionesController", "getSeccionCount");
+$router->addRoute("GET", "/secciones/matricula", "SeccionesController", "getSeccionAsig");
+$router->addRoute("GET", "/secciones/count", "SeccionesController", "getSeccionCount");
+$router->addRoute("POST", "/secciones", "SeccionesController", "createSeccion");
 
 //Routes for Estudiantes
 $router->addRoute("GET", "/matricula/estudiantes/{id}", "MatriculaController", "getEstudiantes");
@@ -47,6 +50,10 @@ $router->addRoute("GET", "/carreras", "CarreraController", "getAllCareers");
 
 //Routes for Centros
 $router->addRoute("GET", "/centros", "CentroController", "getAllCenters");
+
+//Routes for Clases
+$router->addRoute("GET", "/clases", "ClaseController", "getClasesByDepartment");
+$router->addRoute("POST", "/clases", "ClaseController", "createClases");
 
 //Routes
 $router->addRoute("GET", "/solicitud/{id}/estado", "SolicitudController", "getSolicitudEstado");
