a811560def65f666ad6fb6eff05069ac34145c07
Miguel Sánchez <aleminapavon30@gmail.com>
2025-04-13 08:20:28 -0600

    test

--- config/config.php
+++ config/config.php
@@ -1,23 +1,27 @@
 <?php
 function loadEnv($path)
 {
-    if (!file_exists($path)) {
-        throw new Exception(".env file not found at: $path");
+    if (file_exists($path)) {
+        $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+        foreach ($lines as $line) {
+            if (str_starts_with(trim($line), '#')) continue;
+
+            list($key, $value) = explode('=', $line, 2);
+            $key = trim($key);
+            $value = trim($value, " \t\n\r\0\x0B\"'");
+
+            $_ENV[$key] = $value;
+            putenv("$key=$value");
+        }
     }
-
-    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($lines as $line) {
-        if (str_starts_with(trim($line), '#')) continue;
-
-        list($key, $value) = explode('=', $line, 2);
-        $key = trim($key);
-        $value = trim($value, " \t\n\r\0\x0B\"'");
-
-        $_ENV[$key] = $value;
-        putenv("$key=$value");
+    
+    foreach (getenv() as $key => $value) {
+        if (!isset($_ENV[$key])) {
+            $_ENV[$key] = $value;
+        }
     }
 }
 
-return loadEnv(__DIR__ . '/../.env');
-?>
+loadEnv(__DIR__ . '/../.env');
 
+?>
