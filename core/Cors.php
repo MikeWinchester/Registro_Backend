7344dc1c0bb4d14e261f0711dc1207bd8e6ed375
Miguel Sánchez <aleminapavon30@gmail.com>
2025-04-13 19:58:26 -0600

    test 3

--- core/Cors.php
+++ core/Cors.php
@@ -14,13 +14,12 @@ class Cors {
         
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-                header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH");            
+                header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH");
+            
+            if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+            
             exit(0);
         }
-
-        header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, X-CSRF-Token, X-HTTP-Method-Override, Accept, id, estudianteid, seccionid");
-
     }
 }
-
-//{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}
