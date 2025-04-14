1debdcb568dc3a505c7ce3d6a38a0dd66587732c
Miguel Sánchez <aleminapavon30@gmail.com>
2025-04-13 19:03:20 -0600

    updated cors

--- core/Cors.php
+++ core/Cors.php
@@ -17,7 +17,7 @@ class Cors {
                 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS, PATCH");
             
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+                header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}, id, seccionid, estudianteid");
             
             exit(0);
         }
