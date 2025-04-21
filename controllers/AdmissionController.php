fa7b40fd754a1fdb5b1fc763d8047b0e2fcae665
Miguel Sánchez <aleminapavon30@gmail.com>
2025-04-20 22:52:11 -0600

    update

--- controllers/AdmissionController.php
+++ controllers/AdmissionController.php
@@ -27,7 +27,7 @@ class AdmissionController extends BaseController {
     }
 
     public function create($request) {
-        //try {
+        try {
             // 1. Validación básica de campos requeridos
             $requiredFields = [
                 'primerNombre', 'primerApellido', 'email', 'tipoDocumento', 'numeroDocumento', 'codigoPais', 'telefono',
@@ -65,9 +65,9 @@ class AdmissionController extends BaseController {
             // 10. Responder con éxito
             $this->sendSuccessResponse($data, $admissionResult['admision_id']['admision_id'], $codigo);
 
-        //} catch (Exception $e) {
-          //  $this->handleException($e);
-        //}
+        } catch (Exception $e) {
+            $this->handleException($e);
+        }
     }
 
     /* Métodos de validación */
